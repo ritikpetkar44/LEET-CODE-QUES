9593d900dba6069c3ccb9b4bb2dc78ecb3fe7ec7
Yash Petkar <ritikpetkar44@gmail.com>
2026-08-31 11:03:44 +0530

    Time: 0 ms (100%), Space: 138 MB (5.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        if(head==NULL or head->next==NULL or head->next->next==NULL) return {-1,-1};
-        vector<int> help;
-        int idx=1;
-        ListNode *prev=head,*curr=head->next,*next=curr->next;
-        while(next){
-            int a=prev->val,b=curr->val,c=next->val;
-            if((b>a and b>c) or (b<a and b<c)) help.push_back(idx);
-            prev=curr;
-            curr=next;
-            next=next->next;
-            idx++;
-        } 
-        if (help.size()<2) return {-1,-1};
-        int minDiff=INT_MAX;
-        for(int i=1;i<help.size();i++) minDiff=min(minDiff,help[i]-help[i-1]);
-        int maxDiff=help[help.size()-1]-help[0];
-        return {minDiff,maxDiff};
+        vector<int> nums;
+
+        while(head){
+            nums.push_back(head->val);
+            head = head->next;
+        }
+
+        vector<int> criticalPoints;
+
+        int n = nums.size();
+
+        for(int i = 1; i < n - 1; i++){
+            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]){
+                criticalPoints.push_back(i);
+            }
+            else if(nums[i] < nums[i - 1] && nums[i] < nums[i + 1]){
+                criticalPoints.push_back(i);
+            }
+        }
+
+        int m = criticalPoints.size();
+
+        if(m < 2) return {-1, -1};
+
+        int minDist = INT_MAX;
+
+        int maxDist = criticalPoints[m - 1] - criticalPoints[0];
+
+        for(int i = 1; i < m; i++){
+            minDist = min(minDist,
+                criticalPoints[i] - criticalPoints[i - 1]);
+        }
+
+        return {minDist, maxDist};
     }
 };
